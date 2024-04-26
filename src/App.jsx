4366527da26af7f4cff53da6822e101e752328eb
BelloAngela <bellotile@mail.com>
2024-04-25 23:29:09 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState,(0)
+
+  export default function App() {
+    const [count, setCount] = useState(0);
+  
+    useEffect((EMPTY array) => {});
+    console.log("hello from useEffect");
+
+  }
+  [count] );
+    <>
+      
+      
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <>
+          
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
